44824e9ce4ebaa777bd2bcba5ec3eb0c0e7ea7bb
Vlastimir <vlastimir.radojevic@proglove.de>
2023-05-27 00:49:12 +0200

    Adding Swift package support for ConnectSDK

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.5
+import PackageDescription
+
+let package = Package(
+    name: "ConnectSDK",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "ConnectSDK",
+            targets: ["ConnectSDK", "Dependencies"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.16.0"),
+        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", from: "2.0.0"),
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "1.0.0"),
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.16")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ConnectSDK",
+            url: "https://dl.cloudsmith.io/CujHIwqxWVjq8tLK/proglove/markconnectiossdk-dev/raw/files/ConnectSDK.xcframework.zip",
+            checksum: "3de8af8051b30b04090144f94b49bbb024b0780b5ed32e47ca511d1125a8289a"),
+        .target(
+            name: "Dependencies",
+            dependencies: [
+                "ZIPFoundation",
+                "OpenSSL",
+                .target(name: "ConnectSDK"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSAuthCore", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSCognitoIdentityProviderASF", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSIoT", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSMobileClientXCF", package: "aws-sdk-ios-spm")],
+                path: "Sources/ConnectSDK"
+        )
+    ]
+)
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# connect-ios-sdk
+# ProGlove ConnectSDK
+
+The **ProGlove ConnectSDK** is a Swift SDK used to communicate with the ProGlove scanners.
+
+## Licence
+
+This library is licenced under [ProGlove License](/LICENSE).
+
+## Requirements
+
+The ConnectSDK for Swift supports the following:
+- Swift 5.5 or higher
+- iOS 12 or higher
+
+Environment for watchOS is supported by this library:
+**placeholder**
+
+## Dependencies
+
+Dependencies that are being used are necessary for the SDK to work properly and also features like Insight Cloud connection is possible because of them.
+
+Current dependencies that are being used in the package are:
+- SwiftProtobuf
+- ZipFoundation
+- OpenSSL
+- AWS iOS SDK
+
+## API Reference documentation
+
+Please use the [ProGlove Documentation](https://docs.proglove.com/en/insight-mobile--ios-.html) that is already provided for now.
+For now, DocC is not in the current scope, but hopefully in the near future it will be included.
