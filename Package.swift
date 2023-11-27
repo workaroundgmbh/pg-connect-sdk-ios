205ba1e28acdf39090f4c68f815cca1c3b668936
Vlastimir <106685536+vlasty-proglove@users.noreply.github.com>
2023-11-27 14:36:19 +0100

    Update Package.swift with 2.2.0 release

--- Package.swift
+++ Package.swift
@@ -13,22 +13,22 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.16.0"),
         .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", from: "2.0.0"),
         .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "1.0.0"),
-        .package(url: "https://github.com/unrelentingtech/SwiftCBOR.git", from: "0.4.4"),
-        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.16")
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.16"),
+        .package(url: "https://github.com/NordicSemiconductor/IOS-nRF-Connect-Device-Manager", from: "1.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "ConnectSDK",
-            url: "https://dl.cloudsmith.io/CujHIwqxWVjq8tLK/proglove/markconnectiossdk-dev/raw/versions/2.1.0/ConnectSDK-2.1.0.xcframework.zip",
-            checksum: "1233e55c2efad431c5ffa74b5c1d773e5e31f824669e9c180bfa7709f6fef646"),
+            url: "https://dl.cloudsmith.io/CujHIwqxWVjq8tLK/proglove/markconnectiossdk-dev/raw/versions/2.2.0/ConnectSDK-2.2.0.xcframework.zip",
+            checksum: "dd19076f2b6c5a5766aa59427ebabc8dbe78577dbf98d5dd1e56eafc35eb828c"),
         .target(
             name: "ConnectSDKDependencies",
             dependencies: [
                 "OpenSSL",
-                "SwiftCBOR",
                 "ZIPFoundation",
                 .target(name: "ConnectSDK"),
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "iOSMcuManagerLibrary", package: "IOS-nRF-Connect-Device-Manager"),
                 .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
                 .product(name: "AWSAuthCore", package: "aws-sdk-ios-spm"),
                 .product(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm"),
