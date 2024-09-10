01b22eb58c101e45a9993b21be2419f0033535fc
Vlastimir Radojevic <vlastimir.radojevic@proglove.de>
2024-09-10 14:24:39 +0200

    Release 2.5.0

--- Package.swift
+++ Package.swift
@@ -12,15 +12,15 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.26.0"),
         .package(url: "https://github.com/workaroundgmbh/aws-sdk-ios-spm", from: "2.36.2"),
-        .package(url: "https://github.com/workaroundgmbh/OpenSSL.git", from: "1.1.3"),
+        .package(url: "https://github.com/workaroundgmbh/OpenSSL.git", from: "1.1.4"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.19"),
-        .package(url: "https://github.com/NordicSemiconductor/IOS-nRF-Connect-Device-Manager", from: "1.6.0")
+        .package(url: "https://github.com/NordicSemiconductor/IOS-nRF-Connect-Device-Manager", .upToNextMinor(from: "1.6.0"))
     ],
     targets: [
         .binaryTarget(
             name: "ConnectSDK",
-            url: "https://dl.cloudsmith.io/QQ43WPa2Y7VlFUM3/proglove/markconnectiossdk-prod/raw/names/ConnectSDK-2.4.0.xcframework/versions/2.4.0/ConnectSDK-2.4.0.xcframework.zip?accept_eula=8",
-            checksum: "4504c3fb001c4ff2d30b97e4aae877c5625be2bfe8c868710d0e2823f63ef635"),
+            url: "https://dl.cloudsmith.io/QQ43WPa2Y7VlFUM3/proglove/markconnectiossdk-prod/raw/names/ConnectSDK-2.5.0.xcframework/versions/2.5.0/ConnectSDK-2.5.0.xcframework.zip?accept_eula=8",
+            checksum: "82856b70725c429df13a4ddc3869298cc3d6bfa59332c1d98c32cebaf8f8db0e"),
         .target(
             name: "ConnectSDKDependencies",
             dependencies: [
