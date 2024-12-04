b0408367381efc001d8dc8ecd6296029b142c6f9
Vlastimir <106685536+vlasty-proglove@users.noreply.github.com>
2024-12-04 12:46:52 +0100

    Release SDK v2.6.0
    Signed-off-by: Vlastimir <106685536+vlasty-proglove@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "ConnectSDK",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "ConnectSDK",
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ConnectSDK",
-            url: "https://dl.cloudsmith.io/QQ43WPa2Y7VlFUM3/proglove/markconnectiossdk-prod/raw/names/ConnectSDK-2.5.0.xcframework/versions/2.5.0/ConnectSDK-2.5.0.xcframework.zip?accept_eula=8",
-            checksum: "82856b70725c429df13a4ddc3869298cc3d6bfa59332c1d98c32cebaf8f8db0e"),
+            url: "https://dl.cloudsmith.io/QQ43WPa2Y7VlFUM3/proglove/markconnectiossdk-prod/raw/names/ConnectSDK-2.6.0.xcframework/versions/2.6.0/ConnectSDK-2.6.0.xcframework.zip?accept_eula=8",
+            checksum: "1ed5168e0560133e98b806d8c054bf0779bb9e4e67bdac1ef53805dac38a7537"),
         .target(
             name: "ConnectSDKDependencies",
             dependencies: [
